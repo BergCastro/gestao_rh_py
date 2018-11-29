f1c1dd66886a7c6aaa1ec96a32f77f529124484d
Lindemberg <lindembergncastro@outlook.com>
2018-11-29 15:34:35 -0300

    add bootstrap

---
+++ apps/funcionarios/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import FuncionariosList, FuncionarioEdit, FuncionarioDelete, FuncionarioCreate
+
+urlpatterns = [
+    path('', FuncionariosList.as_view(), name='list_funcionarios'),
+    path('editar/<int:pk>', FuncionarioEdit.as_view(), name='update_funcionario'),
+    path('deletar/<int:pk>', FuncionarioDelete.as_view(), name='delete_funcionario'),
+    path('create/', FuncionarioCreate.as_view(), name='novo_funcionario'),
+]
